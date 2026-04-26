5eb96dd1eb698b92f8581f4665b45fedc355c8f4
IT24101896 <it24101896@my.sliit.lk>
2026-04-26 22:54:04 +0530

    Delete main directory

--- main/java/com/tourist/backend/dto/LoginRequest.java
+++
@@ -1,9 +0,0 @@
-package com.tourist.backend.dto;
-
-import lombok.Data;
-
-@Data
-public class LoginRequest {
-    private String email;
-    private String password;
-}
--- main/java/com/tourist/backend/service/UserService.java
+++
@@ -1,4 +0,0 @@
-package com.tourist.backend.service;
-
-public class UserService {
-}
